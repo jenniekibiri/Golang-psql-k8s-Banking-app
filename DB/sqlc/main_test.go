1b00ce21d00259838e54d863e5234434d312d895
jenniekibiri <josphatje@gmail.com>
2023-05-14 15:18:06 +0300

    load configs using viper

--- DB/sqlc/main_test.go
+++ DB/sqlc/main_test.go
@@ -6,19 +6,22 @@ import (
 	"log"
 	"os"
 	"testing"
+	"github.com/jenniekibiri/Golang-psql-k8s-Banking-app/util"
+
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:5463@localhost:5432/simple_bank?sslmode=disable"
-)
+
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err :=util.LoadConfig("../..")
+	if err !=nil{
+		log.Fatal("cannot load config: ", err)
+	}
+	
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"database/sql"
+	"log"
+
 	"github.com/jenniekibiri/Golang-psql-k8s-Banking-app/api"
 	db "github.com/jenniekibiri/Golang-psql-k8s-Banking-app/db/sqlc"
+	"github.com/jenniekibiri/Golang-psql-k8s-Banking-app/util"
 	_ "github.com/lib/pq"
-	"log"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:5463@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
